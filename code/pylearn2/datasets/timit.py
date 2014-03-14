abb006cfbbb2910f58ce6c5e56cfa05c4f9e858a
Vincent Dumoulin <vi.dumoulin@gmail.com>
2014-03-14 18:07:52 -0400

    Add TIMITSequences dataset

--- code/pylearn2/datasets/timit.py
+++ code/pylearn2/datasets/timit.py
@@ -9,13 +9,16 @@ __maintainer__ = "Vincent Dumoulin"
 __email__ = "dumouliv@iro"
 
 
-import gc
 import os.path
 import functools
 import numpy
 from pylearn2.utils.iteration import resolve_iterator_class
 from pylearn2.datasets.dataset import Dataset
 from pylearn2.space import CompositeSpace, VectorSpace, IndexSpace
+from research.code.pylearn2.space import (
+    VectorSequenceSpace,
+    IndexSequenceSpace
+)
 from pylearn2.utils import serial
 from pylearn2.utils import safe_zip
 from research.code.scripts.segmentaxis import segment_axis
@@ -28,7 +31,7 @@ class TIMIT(Dataset):
     Frame-based TIMIT dataset
     """
     _default_seed = (17, 2, 946)
-    
+
     # Mean and standard deviation of the acoustic samples from the whole
     # dataset (train, valid, test).
     _mean = 0.0035805809921434142
@@ -406,16 +409,329 @@ class TIMIT(Dataset):
                                      convert=convert)
 
 
+class TIMITSequences(Dataset):
+    """
+    Sequence-based TIMIT dataset
+    """
+    _default_seed = (17, 2, 946)
+
+    # Mean and standard deviation of the acoustic samples from the whole
+    # dataset (train, valid, test).
+    _mean = 0.0035805809921434142
+    _std = 542.48824133746177
+
+    def __init__(self, which_set, frame_length, start=0, stop=None,
+                 audio_only=False, rng=_default_seed):
+        """
+        Parameters
+        ----------
+        which_set : str
+            Either "train", "valid" or "test"
+        frame_length : int
+            Number of acoustic samples contained in the sliding window
+        start : int, optional
+            Starting index of the sequences to use. Defaults to 0.
+        stop : int, optional
+            Ending index of the sequences to use. Defaults to `None`, meaning
+            sequences are selected all the way to the end of the array.
+        audio_only : bool, optional
+            Whether to load only the raw audio and no auxiliary information.
+            Defaults to `False`.
+        rng : object, optional
+            A random number generator used for picking random indices into the
+            design matrix when choosing minibatches.
+        """
+        self.frame_length = frame_length
+        self.audio_only = audio_only
+
+        # RNG initialization
+        if hasattr(rng, 'random_integers'):
+            self.rng = rng
+        else:
+            self.rng = numpy.random.RandomState(rng)
+
+        # Load data from disk
+        self._load_data(which_set)
+        # Standardize data
+        for i, sequence in enumerate(self.raw_wav):
+            self.raw_wav[i] = (sequence - TIMIT._mean) / TIMIT._std
+
+        # Slice data
+        if stop is not None:
+            self.raw_wav = self.raw_wav[start:stop]
+            if not self.audio_only:
+                self.phones = self.phones[start:stop]
+                self.phonemes = self.phonemes[start:stop]
+                self.words = self.words[start:stop]
+        else:
+            self.raw_wav = self.raw_wav[start:]
+            if not self.audio_only:
+                self.phones = self.phones[start:]
+                self.phonemes = self.phonemes[start:]
+                self.words = self.words[start:]
+
+        samples_sequences = []
+        targets_sequences = []
+        phones_sequences = []
+        phonemes_sequences = []
+        words_sequences = []
+        for sequence_id, samples_sequence in enumerate(self.raw_wav):
+            # Sequence segmentation
+            samples_segmented_sequence = segment_axis(samples_sequence,
+                                                      frame_length,
+                                                      frame_length - 1)[:-1]
+            samples_sequences.append(samples_segmented_sequence)
+            targets_sequences.append(samples_sequence[frame_length:].reshape(
+                (samples_sequence[frame_length:].shape[0], 1)
+            ))
+            if not self.audio_only:
+                target_phones = self.phones[sequence_id][frame_length:]
+                phones_sequences.append(target_phones.reshape(
+                    (target_phones.shape[0], 1)
+                ))
+                target_phonemes = self.phonemes[sequence_id][frame_length:]
+                phonemes_sequences.append(target_phonemes.reshape(
+                    (target_phonemes.shape[0], 1)
+                ))
+                target_words = self.words[sequence_id][frame_length:]
+                words_sequences.append(target_words.reshape(
+                    (target_words.shape[0], 1)
+                ))
+
+        del self.raw_wav
+        self.samples_sequences = samples_sequences
+        self.targets_sequences = targets_sequences
+        self.data = [samples_sequences, targets_sequences]
+        if not self.audio_only:
+            del self.phones
+            del self.phonemes
+            del self.words
+            self.phones_sequences = phones_sequences
+            self.phonemes_sequences = phonemes_sequences
+            self.words_sequences = words_sequences
+            self.data.extend([phones_sequences, phonemes_sequences,
+                              words_sequences])
+        self.num_examples = len(samples_sequences)
+
+        # DataSpecs
+        features_space = VectorSequenceSpace(window_dim=self.frame_length)
+        features_source = 'features'
+
+        targets_space = VectorSequenceSpace(window_dim=1)
+        targets_source = 'targets'
+
+        space_components = [features_space, targets_space]
+        source_components = [features_source, targets_source]
+        batch_components = [None, None]
+
+        if not self.audio_only:
+            num_phones = numpy.max([numpy.max(sequence) for sequence
+                                    in self.phones_sequences]) + 1
+            phones_space = IndexSequenceSpace(
+                max_labels=num_phones,
+                window_dim=1,
+                dtype=str(self.phones_sequences[0].dtype)
+            )
+            phones_source = 'phones'
+
+            num_phonemes = numpy.max([numpy.max(sequence) for sequence
+                                      in self.phonemes_sequences]) + 1
+            phonemes_space = IndexSequenceSpace(
+                max_labels=num_phonemes,
+                window_dim=1,
+                dtype=str(self.phonemes_sequences[0].dtype)
+            )
+            phonemes_source = 'phonemes'
+
+            num_words = numpy.max([numpy.max(sequence) for sequence
+                                   in self.words_sequences]) + 1
+            words_space = IndexSequenceSpace(
+                max_labels=num_words,
+                window_dim=1,
+                dtype=str(self.words_sequences[0].dtype)
+            )
+            words_source = 'words'
+
+            space_components.extend([phones_space, phonemes_space,
+                                     words_space])
+            source_components.extend([phones_source, phonemes_source,
+                                     words_source])
+            batch_components.extend([None, None, None])
+
+        space = CompositeSpace(space_components)
+        source = tuple(source_components)
+        self.data_specs = (space, source)
+        self.batch_buffers = batch_components
+
+        # Defaults for iterators
+        self._iter_mode = resolve_iterator_class('shuffled_sequential')
+        self._iter_data_specs = (CompositeSpace((features_space,
+                                                 targets_space)),
+                                 (features_source, targets_source))
+
+    def _fetch_index(self, indexes):
+        digit = numpy.digitize(indexes, self.cumulative_example_indexes) - 1
+        return zip(digit,
+                   numpy.array(indexes) - self.cumulative_example_indexes[digit])
+
+    def _load_data(self, which_set):
+        """
+        Load the TIMIT data from disk.
+
+        Parameters
+        ----------
+        which_set : str
+            Subset of the dataset to use (either "train", "valid" or "test")
+        """
+        # Check which_set
+        if which_set not in ['train', 'valid', 'test']:
+            raise ValueError(which_set + " is not a recognized value. " +
+                             "Valid values are ['train', 'valid', 'test'].")
+
+        # Create file paths
+        timit_base_path = os.path.join(os.environ["PYLEARN2_DATA_PATH"],
+                                       "timit/readable")
+        speaker_info_list_path = os.path.join(timit_base_path, "spkrinfo.npy")
+        phonemes_list_path = os.path.join(timit_base_path,
+                                          "reduced_phonemes.pkl")
+        words_list_path = os.path.join(timit_base_path, "words.pkl")
+        speaker_features_list_path = os.path.join(timit_base_path,
+                                                  "spkr_feature_names.pkl")
+        speaker_id_list_path = os.path.join(timit_base_path,
+                                            "speakers_ids.pkl")
+        raw_wav_path = os.path.join(timit_base_path, which_set + "_x_raw.npy")
+        phonemes_path = os.path.join(timit_base_path,
+                                     which_set + "_x_phonemes.npy")
+        phones_path = os.path.join(timit_base_path,
+                                     which_set + "_x_phones.npy")
+        words_path = os.path.join(timit_base_path, which_set + "_x_words.npy")
+        speaker_path = os.path.join(timit_base_path,
+                                    which_set + "_spkr.npy")
+
+        # Load data. For now most of it is not used, as only the acoustic
+        # samples are provided, but this is bound to change eventually.
+        # Global data
+        if not self.audio_only:
+            self.speaker_info_list = serial.load(
+                speaker_info_list_path
+            ).tolist().toarray()
+            self.speaker_id_list = serial.load(speaker_id_list_path)
+            self.speaker_features_list = serial.load(speaker_features_list_path)
+            self.words_list = serial.load(words_list_path)
+            self.phonemes_list = serial.load(phonemes_list_path)
+        # Set-related data
+        self.raw_wav = serial.load(raw_wav_path)
+        if not self.audio_only:
+            self.phonemes = serial.load(phonemes_path)
+            self.phones = serial.load(phones_path)
+            self.words = serial.load(words_path)
+            self.speaker_id = numpy.asarray(serial.load(speaker_path), 'int')
+
+    def _validate_source(self, source):
+        """
+        Verify that all sources in the source tuple are provided by the
+        dataset. Raise an error if some requested source is not available.
+
+        Parameters
+        ----------
+        source : `tuple` of `str`
+            Requested sources
+        """
+        for s in source:
+            try:
+                self.data_specs[1].index(s)
+            except ValueError:
+                raise ValueError("the requested source named '" + s + "' " +
+                                 "is not provided by the dataset")
+
+    def get_data_specs(self):
+        """
+        Returns the data_specs specifying how the data is internally stored.
+
+        This is the format the data returned by `self.get_data()` will be.
+
+        .. note::
+
+            Once again, this is very hacky, as the data is not stored that way
+            internally. However, the data that's returned by `TIMIT.get()`
+            _does_ respect those data specs.
+        """
+        return self.data_specs
+
+    def get(self, source, indexes):
+        """
+        .. todo::
+
+            WRITEME
+        """
+        if type(indexes) is slice:
+            indexes = numpy.arange(indexes.start, indexes.stop)
+        assert indexes.shape == (1,)
+        self._validate_source(source)
+        rval = []
+        for so in source:
+            rval.append(
+                self.data[self.data_specs[1].index(so)][indexes]
+            )
+        return tuple(rval)
+
+    @functools.wraps(Dataset.iterator)
+    def iterator(self, mode=None, batch_size=None, num_batches=None,
+                 rng=None, data_specs=None, return_tuple=False):
+        """
+        .. todo::
+
+            WRITEME
+        """
+        if data_specs is None:
+            data_specs = self._iter_data_specs
+
+        # If there is a view_converter, we have to use it to convert
+        # the stored data for "features" into one that the iterator
+        # can return.
+        space, source = data_specs
+        if isinstance(space, CompositeSpace):
+            sub_spaces = space.components
+            sub_sources = source
+        else:
+            sub_spaces = (space,)
+            sub_sources = (source,)
+
+        convert = []
+        for sp, src in safe_zip(sub_spaces, sub_sources):
+            convert.append(None)
+
+        # TODO: Refactor
+        if mode is None:
+            if hasattr(self, '_iter_subset_class'):
+                mode = self._iter_subset_class
+            else:
+                raise ValueError('iteration mode not provided and no default '
+                                 'mode set for %s' % str(self))
+        else:
+            mode = resolve_iterator_class(mode)
+
+        if batch_size is None:
+            batch_size = getattr(self, '_iter_batch_size', None)
+        if num_batches is None:
+            num_batches = getattr(self, '_iter_num_batches', None)
+        if rng is None and mode.stochastic:
+            rng = self.rng
+        return FiniteDatasetIterator(self,
+                                     mode(self.num_examples, batch_size,
+                                          num_batches, rng),
+                                     data_specs=data_specs,
+                                     return_tuple=return_tuple,
+                                     convert=convert)
+
+
 if __name__ == "__main__":
-    # train_timit = TIMIT("train", frame_length=240, overlap=10,
-    #                     frames_per_example=5)
-    valid_timit = TIMIT("valid", frame_length=1, overlap=0,
-                        frames_per_example=100, audio_only=False)
-    # test_timit = TIMIT("test", frame_length=240, overlap=10,
-    #                     frames_per_example=5)
-    # import pdb; pdb.set_trace()
-    data_specs = (IndexSpace(max_labels=62, dim=1, dtype='int16'),
-                  'phones')
-    it = valid_timit.iterator(mode='random_uniform', data_specs=data_specs,
-                              num_batches=100, batch_size=256)
-    it.next()
+    valid_timit = TIMITSequences("valid", frame_length=100, audio_only=False)
+    data_specs = (CompositeSpace([VectorSequenceSpace(window_dim=100),
+                                  VectorSequenceSpace(window_dim=1),
+                                  VectorSequenceSpace(window_dim=62)]),
+                  ('features', 'targets', 'phones'))
+    it = valid_timit.iterator(mode='sequential', data_specs=data_specs,
+                              num_batches=10, batch_size=1)
+    for rval in it:
+        print [val.shape for val in rval]
